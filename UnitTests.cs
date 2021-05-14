1729342097cdbd56392b37bbc3e6605d1adc5b48
Krisjanis Mezs <krisjanis.mezs@gmail.com>
2021-05-14 18:30:22 +0300

    Minor changes.

--- UnitTests.cs
+++ UnitTests.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 namespace GameOfLife
 {
-    public class EmptyClass
+    public class UnitTests
     {
-        public EmptyClass()
+        public UnitTests()
         {
+            // Empty board
+
+            int[,] init_test0 = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+
+            int[,] expected_test0 = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+
+            // Single live cell in midle
+
+            int[,] init_test1 = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
+
+            int[,] expected_test1 = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+
+            // Single live cell in midle
+
+            int[,] init_test2 = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
+
+            int[,] expected_test2 = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
         }
     }
 }
